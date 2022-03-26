674168c707cfeecaa9871e3084412313bb3e18ae
gityasu2021 <nimcknfd0320@gmail.com>
2022-03-26 12:37:58 +0900

    変更

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ $(function(){
     hamburger();
   });
   // メニューのリンクをクリックした時
-  $('#navi a').on('click', function() {
+  $('.header__nav a').on('click', function() {
     // ハンバーガーメニューの共通処理を呼び出す
     hamburger();
   });
@@ -18,11 +18,11 @@ $(function(){
   スムーススクロール
   ===================================================*/
   // ページ内リンクのイベント
-  $('a[href^="#"]').click(function(){
+  $('a[href^="."]').click(function(){
     // リンクを取得
     let href= $(this).attr("href");
     // ジャンプ先のid名をセット
-    let target = $(href == "#" || href == "" ? 'html' : href);
+    let target = $(href == "." || href == "" ? 'html' : href);
     // トップからジャンプ先の要素までの距離を取得
     let position = target.offset().top;
     // animateでスムーススクロールを行う
@@ -31,7 +31,7 @@ $(function(){
     return false;
   });
 
-  /*=================================================
+ /*=================================================
   フェード表示
   ===================================================*/
   $(".inview").on("inview", function (event, isInView) {
@@ -40,7 +40,6 @@ $(function(){
       $(this).stop().addClass("show");
     }
   });
-
   /*=================================================
   スクロール時のイベント
   ===================================================*/
@@ -59,12 +58,12 @@ $(function(){
     // スクロール位置が520pxを超えた場合
     if (scroll > 520) {
       // ロゴとハンバーガ―メニュをfadeInで表示する
-      $('.logo').fadeIn(500);
+      $('.header__title').fadeIn(500);
       $('.hamburger').fadeIn(500);
     // スクロール位置が520px未満の場合
     } else {
       // ロゴとハンバーガ―メニュをfadeOutで非表示にする
-      $('.logo').fadeOut(500);
+      $('.header__title').fadeOut(500);
       $('.hamburger').fadeOut(500);
     }
 
@@ -72,9 +71,9 @@ $(function(){
     サイドボタンを表示
     ===================================================*/
     // 画面下から#galleryまでの距離を取得
-    let gallery_position = $('#gallery').offset().top - $(window).height();
+    let gallery_position = $('.gallery').offset().top - $(window).height();
     // 画面下から#accessまでの距離を取得
-    let access_position = $('#access').offset().top - $(window).height();
+    let access_position = $('.access').offset().top - $(window).height();
 
     // window.innerWidthで画面幅を取得
     // PC表示の場合の処理（画面幅が900pxより大きい場合　※900pxはCSSのブレークポイントとあわせる）
@@ -83,18 +82,18 @@ $(function(){
       if(scroll > gallery_position){
         // #accessが表示されるまでの間は、#side-btnを横からスライドさせて表示する
         if(scroll < access_position){
-          $('#side-btn').css({
+          $('.side-btn').css({
             'transform': 'rotate(-90deg) translateY(0)'
           });
         // #accessが表示されたら、#side-btnをスライドさせて非表示にする
         } else {
-          $('#side-btn').css({
+          $('.side-btn').css({
             'transform': 'rotate(-90deg) translateY(60px)'
           });
         }
       // #galleryが表示される前は、#side-btnをスライドさせて非表示にする
       } else {
-        $('#side-btn').css({
+        $('.side-btn').css({
           'transform': 'rotate(-90deg) translateY(60px)'
         });
       }
@@ -104,7 +103,7 @@ $(function(){
     Accessの背景画像を表示
     ===================================================*/
     // 画面下から#contactまでの距離を取得
-    let contact_position = $('#contact').offset().top - $(window).height();
+    let contact_position = $('.contact').offset().top - $(window).height();
 
     // #accessが表示された場合
     if(scroll > access_position){
@@ -147,10 +146,10 @@ function hamburger() {
 
   if ($('.hamburger').hasClass('active')) {
     // hamburgerクラスにactiveクラスが存在する場合は、naviにもactiveクラスを追加する
-    $('#navi').addClass('active');
+    $('.header__nav').addClass('active');
   } else {
     // hamburgerクラスにactiveクラスが存在しない場合は、naviからactiveクラスを削除する
-    $('#navi').removeClass('active');
+    $('.header__nav').removeClass('active');
   }
 }
 
@@ -163,14 +162,14 @@ function mv_scale(scroll) {
   if (window.innerWidth > 900) {
     // メインビジュアルのCSS（width）を変更する
     // widthの値をスクロール量にあわせて増やすことで画像を拡大させる
-    $('#mainvisual img').css({
+    $('.mainvisual img').css({
       'width': 100/3 + scroll/10  + '%'
     });
   // スマホ表示の場合の処理（画面幅が900px以下の場合）
   } else {
     // メインビジュアルのCSS（width）を変更する
     // widthの値をスクロール量にあわせて減らすことで画像を縮小させる
-    $('#mainvisual img').css({
+    $('.mainvisual img').css({
       'width': 100 - scroll/10  + '%'
     });
   }
